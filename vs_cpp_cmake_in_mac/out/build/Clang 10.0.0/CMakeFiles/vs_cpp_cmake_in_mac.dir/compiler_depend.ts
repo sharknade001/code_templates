68df10367990b89c2c392891c70e2659eba8abd6
sharknade <sharknade@163.com>
2024-11-04 13:59:24 +0800

    rename

---
+++ vs_cpp_cmake_in_mac/out/build/Clang 10.0.0/CMakeFiles/vs_cpp_cmake_in_mac.dir/compiler_depend.ts
@@ -0,0 +1,2 @@
+# CMAKE generated file: DO NOT EDIT!
+# Timestamp file for compiler generated dependencies management for vs_cpp_cmake_in_mac.
---
+++ vs_cpp_cmake_in_mac/out/build/Clang 10.0.0/CMakeFiles/vs_cpp_cmake_in_mac.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for vs_cpp_cmake_in_mac.
+# This may be replaced when dependencies are built.
