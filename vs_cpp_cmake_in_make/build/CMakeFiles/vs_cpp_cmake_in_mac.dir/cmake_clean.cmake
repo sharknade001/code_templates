0a003672a06225741c15899ba57a489f9038e42f
sharknade <sharknade@163.com>
2024-11-04 11:42:56 +0800

    vscode cmake in mac done !

---
+++ vs_cpp_cmake_in_make/build/CMakeFiles/vs_cpp_cmake_in_mac.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vs_cpp_cmake_in_mac.dir/main.cpp.o"
+  "CMakeFiles/vs_cpp_cmake_in_mac.dir/main.cpp.o.d"
+  "vs_cpp_cmake_in_mac"
+  "vs_cpp_cmake_in_mac.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vs_cpp_cmake_in_mac.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
